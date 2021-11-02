5634aaed4a9f95eb0d2dca91af251fc432a00148
GulyaIbrahimova <gulumecidova@gmail.com>
2021-11-01 22:58:39 -0700

    BasicNAvigations in day01_selenium_intro

--- src/test/java/com/cybertek/tests/day01_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cybertek/tests/day01_selenium_intro/BasicNavigations.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigations {
+
     public static void main(String[] args) throws InterruptedException {
         //1.setup browser driver
         WebDriverManager.chromedriver().setup();
